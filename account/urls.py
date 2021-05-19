4c24e9ad21ca82df839f44e7faa2348d09348f39
Joseph Dubon <josephdubon@pm.me>
2021-05-18 17:30:34 -0700

    Added urls for login, logout, dashboard, and user account managment

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,18 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 
-url_patterns = [
-    # post views
-    path('login/', views.user_login, name='login'),
+urlpatterns = [
+    # Login
+    path('login/',
+         auth_views.LoginView.as_view(),
+         name='login'),
+    path('logout/',
+         auth_views.LogoutView.as_view(),
+         name='logout'),
+    path('',
+         views.dashboard,
+         name='dashboard'),
+    path('',
+         include('django.contrib.auth.urls'))
 ]
