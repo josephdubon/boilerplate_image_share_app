44204278fb9e50ea5039a07eeeef3f512a31b425
Joseph Dubon <josephdubon@pm.me>
2021-05-25 16:26:19 -0700

    Registered Image model to admin

--- images/admin.py
+++ images/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Image
 
-# Register your models here.
+
+# Image model
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'slug',
+        'image',
+        'created'
+    ]
+    list_filter = [
+        'created'
+    ]
