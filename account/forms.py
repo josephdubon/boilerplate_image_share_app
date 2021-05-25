bd6d61868487081e996ee2000e5232fd9efb6b62
Joseph Dubon <josephdubon@pm.me>
2021-05-25 12:15:20 -0700

    Created UserEditForm form and ProfileEditForm form

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 # Login form
@@ -32,3 +33,24 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords do not match.')
         return cd['password2']
+
+
+# Edit user form
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'email',
+        )
+
+
+# Edit user profile form
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = (
+            'date_of_birth',
+            'photo',
+        )
