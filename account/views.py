9729763a3d7f1d530df7a52c1a79f466e8d25832
Joseph Dubon <josephdubon@pm.me>
2021-05-25 12:31:11 -0700

    Added message logic for user on success and fail edit user/profile form

--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
@@ -100,6 +101,13 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            # Send message to user on success
+            messages.success(request,
+                             'Profile updated successfully')
+        else:
+            # Send message to user on fail
+            messages.error(request,
+                           'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(
