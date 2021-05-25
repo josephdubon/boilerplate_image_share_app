5f1ea6a89609079508ea6c47bafe67320ae5bc97
Joseph Dubon <josephdubon@pm.me>
2021-05-25 10:50:21 -0700

    Created register view for new_user creation

--- account/views.py
+++ account/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 
 # Login view
@@ -47,4 +47,32 @@ def dashboard(request):
                   'account/dashboard.html',
                   {
                       'section': 'dashboard'
+                  })
+
+
+# User registration view
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user obj but don't save yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            # For security reasons, instead of saving the raw password entered by the
+            # - user, you use the set_password() method of the user model that handles hashing.
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+            # Save the user obj
+            new_user.save()
+            return render(request,
+                          'account/register_done.html',
+                          {
+                              'new_user': new_user
+                          })
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,
+                  'account/register.html',
+                  {
+                      'user_form': user_form
                   })
