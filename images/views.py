60a51641c5657888d9a4750aa3dd95c97516b234
Joseph Dubon <josephdubon@pm.me>
2021-05-26 16:59:35 -0700

    Created view: image_detail and image_like

--- images/views.py
+++ images/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+
+import common.decorators
+from common.decorators import ajax_required
 
 from .forms import ImageCreateForm
+from .models import Image
 
 
 # Submit image form
@@ -31,3 +38,34 @@ def image_create(request):
                       'section': 'images',
                       'form': form
                   })
+
+
+# Bookmark image detail view
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request,
+                  'images/image/detail.html',
+                  {
+                      'section': 'images',
+                      'image': image,
+                  })
+
+
+# Image like/unlike view
+@ajax_required  # Custom common decorator
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except:
+            pass
+    return JsonResponse({'status': 'error'})
