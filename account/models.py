1647e3000d7642531b9c019d04cc6b13282572da
Joseph Dubon <josephdubon@pm.me>
2021-06-19 10:19:55 -0700

    Added contact model and user_model to add fields to Django User model dynamically

--- account/models.py
+++ account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
 
 # Custom user profile
@@ -14,3 +15,33 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'Profile for user {self.user.username}'
+
+
+# Contact
+class Contact(models.Model):
+    user_from = models.ForeignKey('auth.User',
+                                  related_name='rel_from_set',
+                                  on_delete=models.CASCADE)
+    user_to = models.ForeignKey('auth.User',
+                                related_name='rel_to_set',
+                                on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True,
+                                   db_index=True)  # this will create a db index for the created field
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'{self.user_from} follows {self.user_to}'
+
+
+# Add the following fields to User dynamically
+# Retrieve the user model
+user_model = get_user_model()
+# Monkey patch to the User model with add_to_class
+# **never alter the existing Django User Model, this is just for practice
+user_model.add_to_class('following',
+                        models.ManyToManyField('self',
+                                               through=Contact,
+                                               related_name='followers',
+                                               symmetrical=False))
