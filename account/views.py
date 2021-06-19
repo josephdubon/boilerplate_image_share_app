c76ed062835d34b0127a6fd4d6870f8561b6d949
Joseph Dubon <josephdubon@pm.me>
2021-06-19 10:22:10 -0700

    Added new views: user_list, user_detail, user follow

--- account/views.py
+++ account/views.py
@@ -1,8 +1,11 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from common.decorators import ajax_required
 
 from .forms import (
     LoginForm,
@@ -10,7 +13,7 @@ from .forms import (
     UserEditForm,
     ProfileEditForm
 )
-from .models import Profile
+from .models import Profile, Contact
 
 
 # Login view
@@ -117,3 +120,51 @@ def edit(request):
                   'account/edit.html',
                   {'user_form': user_form,
                    'profile_form': profile_form})
+
+
+# User list
+@login_required
+def user_list(request):
+    # Get all active users
+    users = User.objects.filter(is_active=True)
+    return render(request,
+                  'account/user/list.html',
+                  {'section': 'people',
+                   'users': users})
+
+
+# User detail
+@login_required
+def user_detail(request, username):
+    # Get requested user
+    user = get_object_or_404(User,
+                             username=username,
+                             is_active=True)
+    return render(request,
+                  'account/user/detail.html',
+                  {'section': 'people',
+                   'user': user})
+
+
+# User follow
+@ajax_required
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            # Toggle follow
+            if action == 'follow':
+                Contact.objects.get_or_create(user_from=request.user,
+                                              user_to=user)
+            else:
+                # ..and unfollow
+                Contact.objects.filter(user_from=request.user,
+                                       user_to=user).delete()
+            return JsonResponse({'status': 'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'error'})
+    return JsonResponse({'status': 'error'})
