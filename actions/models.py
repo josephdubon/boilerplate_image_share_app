a51853e12550033738c2e255695187d030deffea
Joseph Dubon <josephdubon@pm.me>
2021-06-20 13:10:33 -0700

    Updated target_ct, target_id, and target to Action model

--- actions/models.py
+++ actions/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 # User activities stream
@@ -10,6 +12,18 @@ class Action(models.Model):
                              on_delete=models.CASCADE)
     # The verb describing the action that the user has performed
     verb = models.CharField(max_length=255)
+    # ForeignKey field that points to the ContentType model
+    target_ct = models.ForeignKey(ContentType,
+                                  blank=True,
+                                  null=True,
+                                  related_name='target_obj',
+                                  on_delete=models.CASCADE)
+    # PositiveIntegerField for storing the primary key of the related object
+    target_id = models.PositiveIntegerField(null=True,
+                                            blank=True,
+                                            db_index=True)
+    # A GenericForeignKey field to the related object based on the combination of the two previous fields
+    target = GenericForeignKey('target_ct', 'target_id')
     # The date and time when this action was created
     created = models.DateTimeField(auto_now_add=True,
                                    db_index=True)
