702817a89e39de8836ee9d42f3469eb079b9ae40
Joseph Dubon <josephdubon@pm.me>
2021-06-20 13:28:38 -0700

    Implemented Actions logic

--- account/views.py
+++ account/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from common.decorators import ajax_required
+from actions.utils import create_action
+from actions.models import Action
 
 from .forms import (
     LoginForm,
@@ -54,11 +56,19 @@ def user_login(request):
 # Check if current user is authenticated
 @login_required
 def dashboard(request):
+    # Display all actions by default
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id',
+                                                       flat=True)
+    if following_ids:
+        # If user is following others, retrieve only their actions
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions.select_related('user', 'user__profile') \
+                  .prefetch_related('target')[:10]
     return render(request,
                   'account/dashboard.html',
-                  {
-                      'section': 'dashboard'
-                  })
+                  {'section': 'dashboard',
+                   'actions': actions})
 
 
 # User registration view
@@ -77,6 +87,7 @@ def register(request):
             new_user.save()
             # Create the user an empty profile
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request,
                           'account/register_done.html',
                           {
@@ -160,6 +171,7 @@ def user_follow(request):
             if action == 'follow':
                 Contact.objects.get_or_create(user_from=request.user,
                                               user_to=user)
+                create_action(request.user, 'is following', user)
             else:
                 # ..and unfollow
                 Contact.objects.filter(user_from=request.user,
